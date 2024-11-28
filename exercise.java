89c024d88f35d113d513e97e3dd8f2212d615003
Celestine <celestine4321@gmail.com>
2024-11-28 09:31:49 +0100

    Cleared some unneccesary comments

--- exercise.java
+++ exercise.java
@@ -9,25 +9,3 @@ public class exercise{
     System.out.println("a+b= " + (a+b));
 }
 }
-// // }
-
-// import javax.swing.JOptionPane;
-
-// public class GUICLASSWORK {
-//     public static void main(String[] args) {
-//         String FirstDept;
-//         FirstDept = JOptionPane.showInputDialog("Enter Department 1:");
-//         String SecondDept;
-//         SecondDept = JOptionPane.showInputDialog("Enter Department 2:");
-//         String ThirdDept;
-//         ThirdDept = JOptionPane.showInputDialog("Enter yor Department 3:");
-//         String FourthDept;
-//         FourthDept = JOptionPane.showInputDialog("Enter yor Department 4:");
-
-//         String Departments;
-//         Departments = FirstDept + ", " + SecondDept + ", " + ThirdDept + " and " + FourthDept + ".";
-//         JOptionPane.showMessageDialog(null, "Departments in Computer Science and Mathematics are: " + Departments);
-
-
-//     }
-// }
